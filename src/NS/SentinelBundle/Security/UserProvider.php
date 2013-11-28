da934ff4c4e0cc0821a1fa22ab679824e2672783
Nathanael D. Noblet <nathanael@noblet.ca>
2013-11-28 11:44:08 -0700

    added use statements so the provider works

--- src/NS/SentinelBundle/Security/UserProvider.php
+++ src/NS/SentinelBundle/Security/UserProvider.php
@@ -6,7 +6,7 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 use \Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-
+use Doctrine\Common\Persistence\ObjectManager;
 use NS\SentinelBundle\Entity\User;
 
 /**
