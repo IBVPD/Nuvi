e47be3ce8c46e8e762ca9677bc42de22e20e0984
Nathanael D. Noblet <nathanael@noblet.ca>
2013-11-28 11:44:45 -0700

    added the basic menu

---
+++ src/NS/SentinelBundle/Menu/Builder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace NS\SentinelBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\Security\Core\SecurityContext;
+
+/**
+ * Description of Builder
+ *
+ * @author gnat
+ */
+class Builder 
+{
+    private $factory;
+    protected $securityContext;
+
+    /**
+     * @param FactoryInterface $factory
+     * @param SecurityContext $securityContext
+     */
+    public function __construct(FactoryInterface $factory, SecurityContext $securityContext)
+    {
+        $this->factory         = $factory;
+        $this->securityContext = $securityContext;
+    }
+    
+    public function userMenu()
+    {
+        $menu = $this->factory->createItem('root')->setExtra('translation_domain', 'NSSentinelBundle');
+        $menu->setChildrenAttribute('class','nav');
+        $menu->addChild('NUVI');
+        if( $this->securityContext->isGranted('IS_AUTHENTICATED_FULLY') )
+        {
+            $menu->addChild('Data Entry', array('label'=> 'menu.data-entry'));
+            $menu->addChild('Reports', array('label'=> 'menu.data-reports'));
+            $menu->addChild('Admin', array('label'=> 'menu.data-admin'));
+            $menu->addChild('Logout',array('route' => 'logout'))->setExtra('translation_domain', 'NSSentinelBundle');
+        }
+        else
+            $menu->addChild('Login', array('route' => 'login'))->setExtra('translation_domain', 'NSSentinelBundle');
+
+        return $menu;
+    }    
+}
