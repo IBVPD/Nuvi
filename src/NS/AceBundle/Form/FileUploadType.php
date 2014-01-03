9f5ceeae8849ea8f55007a10921debe25277f18a
Mark <mark.woynarski@nobletdesign.com>
2014-01-03 16:30:04 -0600

    Added file upload field type

---
+++ src/NS/AceBundle/Form/FileUploadType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace NS\AceBundle\Form;
+
+use \Symfony\Component\Form\AbstractType;
+use \Symfony\Component\Form\FormView;
+use \Symfony\Component\Form\FormInterface;
+use \Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Description of SwitchType
+ *
+ * @author gnat
+ */
+class FileUploadType extends AbstractType
+{
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+        $resolver->setDefaults( array('multiple' => false));
+
+        $resolver->setAllowedValues(array('multiple'=>  array(true, false));
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+
+//        if($options['multiple'])
+//            $view->vars['attr']['multiple'] = true;
+    }
+
+    public function getName()
+    {
+        return 'fileupload';
+    }
+}
--- src/NS/AceBundle/Resources/public/js/nsbootstrap.js
+++ src/NS/AceBundle/Resources/public/js/nsbootstrap.js
@@ -2,4 +2,57 @@ $(document).ready(function() {
     $('.date-picker').datepicker({autoclose:true}).next().on(ace.click_event, function(){
         $(this).prev().focus();
     });
+
+    $('input[type=file]:not([multiple])').ace_file_input({
+        no_file:'No File ...',
+        btn_choose:'Choose',
+        btn_change:'Change',
+        droppable:false,
+        onchange:null,
+        thumbnail:false //| true | large
+        //whitelist:'gif|png|jpg|jpeg'
+        //blacklist:'exe|php'
+        //onchange:''
+        //
+    });
+
+    $('input[type=file][multiple]').ace_file_input({
+        style:'well',
+        btn_choose:'Drop files here or click to choose',
+        btn_change:null,
+        no_icon:'icon-cloud-upload',
+        droppable:true,
+        thumbnail:'small'//large | fit
+        //,icon_remove:null//set null, to hide remove/reset button
+        /**,before_change:function(files, dropped) {
+            //Check an example below
+            //or examples/file-upload.html
+            return true;
+        }*/
+        /**,before_remove : function() {
+            return true;
+        }*/
+        ,
+        preview_error : function(filename, error_code) {
+            //name of the file that failed
+            //error_code values
+            //1 = 'FILE_LOAD_FAILED',
+            //2 = 'IMAGE_LOAD_FAILED',
+            //3 = 'THUMBNAIL_FAILED'
+            //alert(error_code);
+        }
+
+    }).on('change', function(){
+        //console.log($(this).data('ace_input_files'));
+        //console.log($(this).data('ace_input_method'));
+    });
+
+    $('#modal-form input[type=file]').ace_file_input({
+        style:'well',
+        btn_choose:'Drop files here or click to choose',
+        btn_change:null,
+        no_icon:'icon-cloud-upload',
+        droppable:true,
+        thumbnail:'large'
+    })
 });
