dc4ad6dbf0f850c84af9dcfd0ba13628ba7b6cc8
Mark <mark.woynarski@nobletdesign.com>
2014-01-08 12:29:26 -0600

    Drag and Drop multi file upload

--- src/NS/AceBundle/Resources/public/js/nsbootstrap.js
+++ src/NS/AceBundle/Resources/public/js/nsbootstrap.js
@@ -16,43 +16,22 @@ $(document).ready(function() {
         //
     });
 
-    $('input[type=file][multiple]').ace_file_input({
-        style:'well',
-        btn_choose:'Drop files here or click to choose',
-        btn_change:null,
-        no_icon:'icon-cloud-upload',
-        droppable:true,
-        thumbnail:'small'//large | fit
-        //,icon_remove:null//set null, to hide remove/reset button
-        /**,before_change:function(files, dropped) {
-            //Check an example below
-            //or examples/file-upload.html
-            return true;
-        }*/
-        /**,before_remove : function() {
-            return true;
-        }*/
-        ,
-        preview_error : function(filename, error_code) {
-            //name of the file that failed
-            //error_code values
-            //1 = 'FILE_LOAD_FAILED',
-            //2 = 'IMAGE_LOAD_FAILED',
-            //3 = 'THUMBNAIL_FAILED'
-            //alert(error_code);
-        }
-
-    }).on('change', function(){
-        //console.log($(this).data('ace_input_files'));
-        //console.log($(this).data('ace_input_method'));
+    $('div.nsFileUpload div.nsUploader').each(function(i, el)
+    {
+        $(el).after('\
+            <div class="ace-file-input">\
+                <label class="file-label" data-title="Browse">\
+                    <span class="file-name" data-title="Drop file here or click &quot;Browse&quot;...">\
+                        <i class="icon-upload-alt"></i>\
+                    </span>\
+                </label>\
+            </div>');
+        new PunkAveFileUploader({
+            'uploadUrl': $(el).data('uploadUrl'),
+            'viewUrl': $(el).data('uploadUrl'),
+            'el': $(el),
+            'existingFiles': [],
+            'delaySubmitWhileUploading': '.edit-form'
+        });
     });
-
-    $('#modal-form input[type=file]').ace_file_input({
-        style:'well',
-        btn_choose:'Drop files here or click to choose',
-        btn_change:null,
-        no_icon:'icon-cloud-upload',
-        droppable:true,
-        thumbnail:'large'
-    })
 });
