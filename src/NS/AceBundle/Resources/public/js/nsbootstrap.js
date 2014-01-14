f3c2d9a5143516c0faabf3415293a3430a73bb0b
Mark <mark.woynarski@nobletdesign.com>
2014-01-14 11:15:16 -0600

    Added masked text input field types

--- src/NS/AceBundle/Resources/public/js/nsbootstrap.js
+++ src/NS/AceBundle/Resources/public/js/nsbootstrap.js
@@ -45,5 +45,13 @@ $(document).ready(function() {
         $(el).ace_spinner($(el).data('options'));
     });
 
+    $('input.nsMasked').each(function(i, el)
+    {
+        $.extend($.mask.definitions, $(el).data('definitions'));
+        
+        $(el).mask($(el).data('mask'), {placeholder:$(el).data('placeholder')});
+        $(el).parents('div.form-group').children('label').append(' <small class="text-info">'+$(el).data('mask')+'</small>');
+    });
+
     $('input.nsKnob').knob();
 });
