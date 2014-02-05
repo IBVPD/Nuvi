5db1dcb516e2d47f7234e4a44e9b1f7b0328518e
Nathanael D. Noblet <nathanael@noblet.ca>
2014-02-05 15:38:13 -0700

    layout fixes

--- src/NS/SentinelBundle/Listeners/ResultPerPage.php
+++ src/NS/SentinelBundle/Listeners/ResultPerPage.php
@@ -36,7 +36,6 @@ class ResultPerPage
             if(!$this->form->isValid())
                 die("FORM IS INVALID! ".$this->form->getErrorsAsString()." Requests ".print_r($request->request->all(),true));
 
-//            die(print_r($this->form->getData(),true));
             $request->getSession()->set('result_per_page',$this->form->get('recordsperpage')->getData());
             $r = new \Symfony\Component\HttpFoundation\RedirectResponse($this->router->generate($this->form->get('target')->getData()));
             $event->setResponse($r);
