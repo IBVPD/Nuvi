ee797e59933d973eb81a99ff7802a2f26a14da26
Nathanael d. Noblet <nathanael@gnat.ca>
2014-02-10 22:35:14 -0700

    Remove un-needed use statements

--- src/NS/SentinelBundle/Controller/UserController.php
+++ src/NS/SentinelBundle/Controller/UserController.php
@@ -2,10 +2,7 @@
 namespace NS\SentinelBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Request;
-use NS\SentinelBundle\Form\MeningitisType;
 
 /**
  * Description of UserController
