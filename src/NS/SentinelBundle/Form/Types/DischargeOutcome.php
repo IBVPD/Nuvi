851f004977ce4c60c85359c5611ca5d7901076a9
Nathanael d. Noblet <nathanael@gnat.ca>
2014-03-04 21:52:39 -0700

    field values

--- src/NS/SentinelBundle/Form/Types/DischargeOutcome.php
+++ src/NS/SentinelBundle/Form/Types/DischargeOutcome.php
@@ -10,10 +10,20 @@ use NS\UtilBundle\Form\Types\ArrayChoice;
  */
 class DischargeOutcome extends ArrayChoice
 {
-    const FIRST_VALUE = 1;
+    const DISCHARGE_ALIVE_WITHOUT_SEQUELAE = 1;
+    const DISCHARGE_ALIVE_WITH_SEQUELAE    = 2;
+    const DIED                             = 3;
+    const TRANSFERRED                      = 4;
+    const LEFT_AGAINST_ADVICE              = 5;
+    const UNKNOWN                          = 99;
 
     protected $values = array(
-                                self::FIRST_VALUE => 'First Value',
+                                self::DISCHARGED_ALIVE_WITHOUT_SEQUELAE => 'Discharged alive, without sequelae',
+                                self::DISCHARGED_ALIVE_WITH_SEQUELAE    => 'Discharged alive, with sequelae',
+                                self::DIED                              => 'Died',
+                                self::TRANSFERRED                       => 'Transferred',
+                                self::LEFT_AGAINST_ADVICE               => 'Left/Discharged against medical advice',
+                                self::UNKNOWN                           => 'Unknown',
                              );
 
     public function getName()
