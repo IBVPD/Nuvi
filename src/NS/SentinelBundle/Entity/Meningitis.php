6ad9ed0fca4964751ee99a01b2dc768cc8ee8a2e
Nathanael d. Noblet <nathanael@gnat.ca>
2014-03-04 21:33:05 -0700

    missing use statements

--- src/NS/SentinelBundle/Entity/Meningitis.php
+++ src/NS/SentinelBundle/Entity/Meningitis.php
@@ -13,8 +13,9 @@ use NS\SentinelBundle\Form\Types\Gender;
 use NS\SentinelBundle\Form\Types\MeningitisCaseStatus;
 use NS\SentinelBundle\Form\Types\MeningitisVaccinationReceived;
 use NS\SentinelBundle\Form\Types\MeningitisVaccinationType;
-use NS\SentinelBundle\Form\Types\GramStainOrganism;
 use NS\SentinelBundle\Interfaces\IdentityAssignmentInterface;
+use \NS\SentinelBundle\Form\Types\DischargeClassification;
+use \NS\SentinelBundle\Form\Types\DischargeOutcome;
 
 use Gedmo\Mapping\Annotation as Gedmo;
 use NS\SecurityBundle\Annotation\Secured;
