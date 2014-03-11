39d9f3ad593b5eb210c5b6ab008763a46f5d5ae0
Nathanael d. Noblet <nathanael@noblet.ca>
2014-03-11 16:36:47 -0600

    remove commented code

--- src/NS/ApiBundle/Controller/AuthorizeController.php
+++ src/NS/ApiBundle/Controller/AuthorizeController.php
@@ -27,37 +27,15 @@ class AuthorizeController extends \Symfony\Bundle\FrameworkBundle\Controller\Con
             throw new NotFoundHttpException("Client id parameter {$request->get('client_id')} is missing.");
 
         $clientManager = $this->get('fos_oauth_server.client_manager.default');
-        $client = $clientManager->findClientByPublicId($request->get('client_id'));
+        $client        = $clientManager->findClientByPublicId($request->get('client_id'));
 
         if (!($client instanceof Client))
             throw new NotFoundHttpException("Client {$request->get('client_id')} is not found.".  get_class($client));
 
-//        $user        = $this->getUser();//container->get('security.context')->getToken()->getUser();
         $authorize    = new Authorize();
         $form         = $this->createForm('api_oauth_server_authorize',$authorize);
-//        $formHandler = $this->container->get('ns_apioauth_server.authorize.form_handler');
         $oauthServier = $this->get('fos_oauth_server.server');
-        
-        
-//        $this->form->setData($authorize);
-//
-//        if ($this->request->getMethod() == 'POST')
-//        {
-//            $this->form->bindRequest($this->request);
-//
-//            if ($this->form->isValid())
-//            {
-//                try
-//                {
-//                    $user = $this->context->getToken()->getUser();
-//                    return $this->oauth2->finishClientAuthorization(true, $user, $this->request, null);
-//                }
-//                catch (OAuth2ServerException $e)
-//                {
-//                    return $e->getHttpResponse();
-//                }
-//            }
-//        }
+
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid())
         {
@@ -71,9 +49,6 @@ class AuthorizeController extends \Symfony\Bundle\FrameworkBundle\Controller\Con
             }
         }
 
-        return $this->container->get('templating')->renderResponse('NSApiBundle:Authorize:authorize.html.twig', array(
-            'form' => $form->createView(),
-            'client' => $client,
-        ));
+        return $this->render('NSApiBundle:Authorize:authorize.html.twig', array('form' => $form->createView(),'client' => $client));
     }
 }
