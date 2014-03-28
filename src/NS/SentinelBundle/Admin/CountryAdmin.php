58ef9e0f33d26c1536fa01542586d6bb487d673e
Nathanael d. Noblet <nathanael@noblet.ca>
2014-03-28 14:44:30 -0600

    updated admin template and entity

--- src/NS/SentinelBundle/Admin/CountryAdmin.php
+++ src/NS/SentinelBundle/Admin/CountryAdmin.php
@@ -7,6 +7,7 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
+use NS\SentinelBundle\Form\Types\GAVIEligible;
 
 class CountryAdmin extends Admin
 {
@@ -15,11 +16,17 @@ class CountryAdmin extends Admin
      */
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
+        $type = new GAVIEligible();
+
         $datagridMapper
             ->add('name')
             ->add('code')
             ->add('region')
             ->add('isActive')
+            ->add('tracksPneumonia')
+            ->add('hasReferenceLab')
+            ->add('hasNationalLab')
+            ->add('gaviEligible','doctrine_orm_callback',array('callback'=> array($this,'filterGaviEligible'),'field_type'=>'choice','field_options'=>array('choices'=>$type->getValues(),'multiple'=>true)))
         ;
     }
 
@@ -31,6 +38,10 @@ class CountryAdmin extends Admin
         $listMapper
             ->add('code')
             ->add('name')
+            ->add('gaviEligible')
+            ->add('tracksPneumonia')
+            ->add('hasReferenceLab')
+            ->add('hasNationalLab')
             ->add('isActive')
             ->add('_action', 'actions', array(
                 'actions' => array(
@@ -50,6 +61,12 @@ class CountryAdmin extends Admin
         $formMapper
             ->add('code')
             ->add('name')
+            ->add('tracksPneumonia',null,array('required'=>false))
+            ->add('hasReferenceLab',null,array('required'=>false))
+            ->add('hasNationalLab',null,array('required'=>false))
+            ->add('gaviEligible','GAVIEligible',array('required'=>false))
+            ->add('population')
+            ->add('populationUnderFive')
             ->add('isActive',null,array('required' => false))
             ->add('region')
         ;
@@ -63,9 +80,31 @@ class CountryAdmin extends Admin
         $showMapper
             ->add('code')
             ->add('name')
-            ->add('isActive')
+            ->add('tracksPneumonia')
+            ->add('hasReferenceLab')
+            ->add('hasNationalLab')
+            ->add('gaviEligible','GAVIEligible')
+            ->add('population')
+            ->add('populationUnderFive')
             ->add('region')
             ->add('sites')
         ;
+    }
+
+    public function filterGaviEligible($queryBuilder,$alias,$field,$value)
+    {
+        if(empty($value['value']))
+            return;
+
+        $out = $params = array();
+        foreach($value['value'] as $x => $role)
+        {
+            $out[] = "$alias.gaviEligible = :type$x";
+            $params["type$x"] = $role;
+        }
+
+        $queryBuilder->andWhere("(".implode(" OR ",$out).")")->setParameters($params);
+
+        return true;
     }
 }
--- src/NS/SentinelBundle/Entity/Country.php
+++ src/NS/SentinelBundle/Entity/Country.php
@@ -52,7 +52,6 @@ class Country implements \Serializable
     private $name;
 
     /**
-     *
      * @var GAVIEligible
      * @ORM\Column(name="gaviEligible",type="GAVIEligible",nullable=true)
      */
@@ -69,7 +68,6 @@ class Country implements \Serializable
      * @ORM\Column(name="populationUnderFive",type="integer",nullable=true)
      */
     private $populationUnderFive;
-    
     
     /**
      * @var Site
@@ -96,6 +94,24 @@ class Country implements \Serializable
      * @ORM\OneToMany(targetEntity="RotaVirus",mappedBy="country")
      */
     private $rotavirusCases;
+
+    /**
+     * @var boolean $tracksPneumonia
+     * @ORM\Column(name="tracksPneumonia",type="boolean")
+     */
+    private $tracksPneumonia = true;
+
+    /**
+     * @var boolean $hasReferenceLab
+     * @ORM\Column(name="hasReferenceLab",type="boolean")
+     */
+    private $hasReferenceLab;
+
+    /**
+     * @var boolean $hasNationalLab
+     * @ORM\Column(name="hasNationalLab",type="boolean")
+     */
+    private $hasNationalLab;
 
     /**
      * Set name
@@ -376,6 +392,73 @@ class Country implements \Serializable
         return $this->populationUnderFive;
     }
 
+    /**
+     * Get tracksPneumonia
+     *
+     * @return boolean
+     */
+    public function getTracksPneumonia()
+    {
+        return $this->tracksPneumonia;
+    }
+
+    /**
+     * Set tracksPneumonia
+     *
+     * @param boolean $tracksPneumonia
+     * @return Country
+     */
+    public function setTracksPneumonia($tracksPneumonia)
+    {
+        $this->tracksPneumonia = $tracksPneumonia;
+
+        return $this;
+    }
+
+    /**
+     * Get hasReferenceLab
+     *
+     * @return boolean
+     */
+    public function getHasReferenceLab()
+    {
+        return $this->hasReferenceLab;
+    }
+
+    /**
+     * Get hasNationalLab
+     *
+     * @return boolean
+     */
+    public function getHasNationalLab()
+    {
+        return $this->hasNationalLab;
+    }
+
+    /**
+     * Set hasReferenceLab
+     * 
+     * @param boolean $hasReferenceLab
+     * @return Country
+     */
+    public function setHasReferenceLab($hasReferenceLab)
+    {
+        $this->hasReferenceLab = $hasReferenceLab;
+        return $this;
+    }
+
+    /**
+     * Set hasNationalLab
+     *
+     * @param boolean $hasNationalLab
+     * @return Country
+     */
+    public function setHasNationalLab($hasNationalLab)
+    {
+        $this->hasNationalLab = $hasNationalLab;
+        return $this;
+    }
+
     public function serialize()
     {
         return serialize(array(
@@ -387,6 +470,9 @@ class Country implements \Serializable
             $this->population,
             $this->populationUnderFive,
             $this->region,
+            $this->hasNationalLab,
+            $this->hasReferenceLab,
+            $this->tracksPneumonia,
             ));
     }
 
@@ -399,9 +485,10 @@ class Country implements \Serializable
             $this->gaviEligible,
             $this->population,
             $this->populationUnderFive,
-            $this->region
+            $this->region,
+            $this->hasNationalLab,
+            $this->hasReferenceLab,
+            $this->tracksPneumonia
             ) = unserialize($serialized);
     }
-
-
 }
