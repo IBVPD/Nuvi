fb4d51afdb22206773241275a98cf89fc44694f6
Nathanael d. Noblet <nathanael@noblet.ca>
2014-05-06 15:39:00 -0600

    Initial Behat addition

---
+++ src/NS/SentinelBundle/Features/Context/FeatureContext.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace NS\SentinelBundle\Features\Context;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+use Behat\Symfony2Extension\Context\KernelAwareInterface;
+use Behat\MinkExtension\Context\MinkContext;
+
+use Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+// Use PHP Assertions:
+//   \PHPUnit_Framework_Assert::assertTrue(true,"Have an event log of $arg1 type");
+//
+
+/**
+ * Feature context.
+ */
+class FeatureContext extends BehatContext //MinkContext if you want to test web
+                  implements KernelAwareInterface
+{
+    private $kernel;
+    private $parameters;
+
+    /**
+     * Initializes context with parameters from behat.yml.
+     *
+     * @param array $parameters
+     */
+    public function __construct(array $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Sets HttpKernel instance.
+     * This method will be automatically called by Symfony2Extension ContextInitializer.
+     *
+     * @param KernelInterface $kernel
+     */
+    public function setKernel(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+//
+// Place your definition and hook methods here:
+//
+//    /**
+//     * @Given /^I have done something with "([^"]*)"$/
+//     */
+//    public function iHaveDoneSomethingWith($argument)
+//    {
+//        $container = $this->kernel->getContainer();
+//        $container->get('some_service')->doSomethingWith($argument);
+//    }
+//
+}
