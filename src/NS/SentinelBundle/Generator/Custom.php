59421fd81b7885955312d5eb5e4d99f39d719350
Nathanael d. Noblet <nathanael@noblet.ca>
2014-05-12 14:27:18 -0600

    use ObjectManager instead of EntityManager - proper namespace

--- src/NS/SentinelBundle/Generator/Custom.php
+++ src/NS/SentinelBundle/Generator/Custom.php
@@ -4,7 +4,7 @@ namespace NS\SentinelBundle\Generator;
 
 use Doctrine\ORM\Id\AbstractIdGenerator;
 use NS\SentinelBundle\Interfaces\IdentityAssignmentInterface;
-use Doctrine\ORM\ObjectManager;
+use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ORM\Query\ResultSetMapping;
 use Doctrine\ORM\Query;
 
@@ -30,10 +30,17 @@ class Custom extends AbstractIdGenerator
         $rsm->addEntityResult('NS\SentinelBundle\Entity\Site', 's');
         $rsm->addFieldResult('s', 'currentCaseId', 'currentCaseId');
 
-        $em->beginTransaction();
-        $id = $em->createNativeQuery('SELECT s.currentCaseId FROM sites s WHERE s.id = '.$site->getId(), $rsm)->getResult(Query::HYDRATE_SINGLE_SCALAR);
-        $em->getConnection()->executeUpdate('UPDATE sites SET currentCaseId = currentCaseId +1 WHERE id = :id', array('id'=>$site->getId()));
-        $em->commit();
+        try
+        {
+            $em->beginTransaction();
+            $id = $em->createNativeQuery('SELECT s.currentCaseId FROM sites s WHERE s.id = '.$site->getId(), $rsm)->getResult(Query::HYDRATE_SINGLE_SCALAR);
+            $em->getConnection()->executeUpdate('UPDATE sites SET currentCaseId = currentCaseId +1 WHERE id = :id', array('id'=>$site->getId()));
+            $em->commit();
+        }
+        catch(\Exception $e)
+        {
+            $em->
+        }
 
         return $entity->getFullIdentifier($id);
     }
