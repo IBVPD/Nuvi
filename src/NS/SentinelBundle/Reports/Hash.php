61a05f13324e522ad2e00c15f0379089478b13ed
Nathanael d. Noblet <nathanael@noblet.ca>
2014-05-13 16:17:50 -0600

    moved specimen collection data

---
+++ src/NS/SentinelBundle/Reports/Hash.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace NS\SentinelBundle\Reports;
+
+
+/**
+ * Description of Collector
+ *
+ * @author gnat
+ */
+class Hash extends ArrayCollection
+{
+    private $nodes = array();
+
+    public function add($key, $value)
+    {
+        if(!isset($this->nodes[$key]))
+            $this->nodes[$key] = new Node();
+
+        $this->nodes[$key]->add($value);
+    }
+
+    public function get($key)
+    {
+        if(isset($this->nodes[$key]))
+            return $this->nodes[$key];
+
+        return null;
+    }
+}
