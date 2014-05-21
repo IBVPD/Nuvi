aa3eed01271bc2129d4b53987bc8f13b42104cec
Nathanael d. Noblet <nathanael@gnat.ca>
2014-05-20 22:33:38 -0600

    new drop down

--- src/NS/SentinelBundle/Entity/Types/CXRResult.php
+++ src/NS/SentinelBundle/Entity/Types/CXRResult.php
@@ -1,21 +1,15 @@
 <?php
 
 namespace NS\SentinelBundle\Entity\Types;
-
 use NS\UtilBundle\Entity\Types\ArrayChoice;
 
-/**
- * Description of TripleChoice
- *
- * @author gnat
- */
 class CXRResult extends ArrayChoice
 {
-    protected $convert_class = '\NS\SentinelBundle\Form\Types\CXRResult';
+    protected $convert_class = 'NS\SentinelBundle\Form\Types\CXRResult';
 
     public function getName()
     {
         return 'CXRResult';
-    }
-    
+    }   
 }
+
--- src/NS/SentinelBundle/Form/Types/CXRResult.php
+++ src/NS/SentinelBundle/Form/Types/CXRResult.php
@@ -2,29 +2,20 @@
 
 namespace NS\SentinelBundle\Form\Types;
 
-use JMS\TranslationBundle\Translation\TranslationContainerInterface;
 use NS\UtilBundle\Form\Types\TranslatableArrayChoice;
 
 /**
- * Description of TripleChoice
+ * Description of CXRResult
  *
- * @author gnat
  */
-class CXRResult extends TranslatableArrayChoice implements TranslationContainerInterface
+class CXRResult extends TranslatableArrayChoice
 {
-    const NORMAL       = 0;
-    const CONSISTENT   = 1;
-    const INCONCLUSIVE = 2;
-    const OTHER        = 3;
-    const UNKNOWN      = 99;
+    const FIRST_VALUE = 1;
 
     protected $values = array(
-                            self::NORMAL       => 'Normal', 
-                            self::CONSISTENT   => 'Consistent with Pneumonia', 
-                            self::INCONCLUSIVE => 'Inconclusive',
-                            self::OTHER        => 'Other',
-                            self::UNKNOWN => 'Unknown');
-    
+                                self::FIRST_VALUE => 'First Value',
+                             );
+
     public function getName()
     {
         return 'CXRResult';
