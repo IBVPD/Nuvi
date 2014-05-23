561fd71cd7072159e6966ab8604cb8760f9e4d1a
Nathanael d. Noblet <nathanael@noblet.ca>
2014-05-23 12:22:13 -0600

    updated layout and relations

--- src/NS/SentinelBundle/Form/IBD/BaseLabType.php
+++ src/NS/SentinelBundle/Form/IBD/BaseLabType.php
@@ -24,19 +24,19 @@ class BaseLabType extends AbstractType
                                                                               'attr' => array('data-context-child'=>'sampleType')
                                                                              ))
             ->add('volume',                     'Volume',               array('label'=>'meningitis-rrl-form.volume','required'=>false,
-                                                                              'attr' => array('data-context-parent'=>'sampleType','data-context-value'=>  json_encode(array($values[SampleType::WHOLE],$values[SampleType::PLEURAL])) )
+                                                                              'attr' => array('data-context-parent'=>'sampleType','data-context-value'=>  json_encode(array($values[SampleType::CSF],$values[SampleType::WHOLE],$values[SampleType::PLEURAL])) )
                                                                              ))
             ->add('DNAExtractionDate',          'acedatepicker',        array('label'=>'meningitis-rrl-form.dna-extraction-date','required'=>false,
-                                                                              'attr' => array('data-context-parent'=>'sampleType','data-context-value'=>  json_encode(array($values[SampleType::ISOLATE],$values[SampleType::WHOLE],$values[SampleType::PLEURAL])))
+                                                                              'attr' => array('data-context-parent'=>'sampleType','data-context-value'=>  json_encode(array($values[SampleType::CSF],$values[SampleType::WHOLE],$values[SampleType::PLEURAL],$values[SampleType::ISOLATE])))
                                                                              ))
             ->add('DNAVolume',                  null,                   array('label'=>'meningitis-rrl-form.dna-volume','required'=>false,
-                                                                              'attr' => array('data-context-parent'=>'sampleType','data-context-value'=>  json_encode(array($values[SampleType::ISOLATE],$values[SampleType::WHOLE],$values[SampleType::PLEURAL])))
+                                                                              'attr' => array('data-context-parent'=>'sampleType','data-context-value'=>  json_encode(array($values[SampleType::CSF],$values[SampleType::WHOLE],$values[SampleType::PLEURAL],$values[SampleType::ISOLATE])))
                                                                              ))
             ->add('isolateViable',              'TripleChoice',         array('label'=>'meningitis-rrl-form.isolate-viable','required'=>false,
                                                                               'attr' => array('data-context-parent'=>'sampleType','data-context-value'=> $values[SampleType::ISOLATE])
                                                                              ))
             ->add('isolateType',                'IsolateType',          array('label'=>'meningitis-rrl-form.isolate-type','required'=>false,
-                                                                              'attr' => array('data-context-parent'=>'sampleType','data-context-value'=>  json_encode(array($values[SampleType::ISOLATE],$values[SampleType::WHOLE],$values[SampleType::PLEURAL])))
+                                                                              'attr' => array('data-context-parent'=>'sampleType','data-context-value'=> $values[SampleType::ISOLATE])
                                                                              ))
             ->add('samples',                    'collection',           array('type' => new LabSampleType(),'allow_add' => true,'by_reference'=>false))
         ;
