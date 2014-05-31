bae43c4191815d22ac25b7c339be704611af293d
Nathanael d. Noblet <nathanael@gnat.ca>
2014-05-31 10:12:56 -0600

    initial import bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -52,6 +52,8 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new NS\ImportBundle\NSImportBundle(),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
