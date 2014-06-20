4e54a598f45f21c31e397b8ec58cceb8a0438124
Nathanael d. Noblet <nathanael@noblet.ca>
2014-06-20 15:15:24 -0600

    api user dashboard and cleaned up auth routes and function names

--- src/NS/ApiBundle/Controller/AuthorizeController.php
+++ src/NS/ApiBundle/Controller/AuthorizeController.php
@@ -4,6 +4,7 @@ namespace NS\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -23,7 +24,7 @@ class AuthorizeController extends Controller
     /**
      * @param \Symfony\Component\HttpFoundation\Request $request
      * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/oauth/v2/auth",name="api_login_auth")
+     * @Route("/oauth/v2/auth",name="ApiClientAuthorization")
      */
     public function authorizeAction(Request $request)
     {
@@ -33,7 +34,7 @@ class AuthorizeController extends Controller
         $clientManager = $this->get('fos_oauth_server.client_manager.default');
         $client        = $clientManager->findClientByPublicId($request->get('client_id'));
 
-        if (!$client instanceof \NS\ApiBundle\Entity\Client)
+        if (!$client instanceof Client)
             throw new NotFoundHttpException("Client {$request->get('client_id')} is not found.". get_class($client));
 
         $authorize    = new Authorize();
@@ -58,16 +59,17 @@ class AuthorizeController extends Controller
     }
 
     /**
-     * @Route("/authorize", name="auth")
+     * @Route("/authorize", name="remoteAuthorizationCallback")
      */
-    public function authAction(Request $request)
+    public function remoteAuthorizationCallbackAction(Request $request)
     {
         $em      = $this->get('doctrine.orm.entity_manager');
         $repo    = $em->getRepository('NSApiBundle:Remote');
         $remotes = $repo->findByUser($this->getUser());
 
         // TODO handle more than one remote... how would we know which client_id/secret is being authorized...
-        // Ask the user since they started off the authorization so should know.
+        // Ask the user since they started off the authorization so should know. - Use a session var?
+        // TODO we need to also somehow check that we're using the user the remote is linked to - another point for using a session var
 
         if(count($remotes)>1)
             throw new \Doctrine\ORM\UnexpectedResultException("We really only support one remote per user at the moment");
@@ -81,11 +83,11 @@ class AuthorizeController extends Controller
             return new RedirectResponse($authorizeClient->getAuthenticationUrl());
 
         if( $authorizeClient->getAccessTokenByAuthorizationCode($request->query->get('code')))
-            return $this->redirect($this->generateUrl('authTest'));
+            return $this->redirect($this->generateUrl('ns_api_dashboard'));
     }
 
     /**
-     * @Route("/authorizeTest",name="authTest")
+     * @Route("/test",name="authTest")
      */
     public function authTestAction()
     {
@@ -98,9 +100,33 @@ class AuthorizeController extends Controller
         foreach($tokens as $token)
         {
             $client->setRemote($token);
-            $r[] = $client->fetch('http://nuvi.noblet.ca/api/v1/articles');
+            $r[] = $client->fetch('http://nuvi.noblet.ca/api/v1/test');
         }
 
         return new Response("Returned: <pre>".print_r($r,true)."</pre>");
+    }
+
+    /**
+     * @Route("/oauth/dashboard",name="ns_api_dashboard")
+     * @Template("NSApiBundle:Authorize:userDashboard.html.twig")
+     */
+    public function userDasboardAction()
+    {
+        $em      = $this->get('doctrine.orm.entity_manager');
+//        $clients = $em->getRepository('NSApiBundle:Client')
+//                      ->createQueryBuilder('c')
+//                      ->leftJoin('c.accessTokens','a')
+//                      ->where('a.user = :user')
+//                      ->setParameter('user',$this->getUser())
+//                      ->getQuery()
+//                      ->getResult();
+        $remotes = $em->getRepository('NSApiBundle:Remote')
+                      ->createQueryBuilder('r')
+                      ->where('r.user = :user')
+                      ->setParameter('user',$em->getReference(get_class($this->getUser()),$this->getUser()->getId()))
+                      ->getQuery()
+                      ->getResult();
+
+        return array('clients'=>array(),'remotes'=>$remotes);
     }
 }
