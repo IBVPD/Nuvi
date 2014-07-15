2a2b072cca08bc1f2adeea09cca84c10d4ef87e2
Nathanael d. Noblet <nathanael@noblet.ca>
2014-07-15 14:08:18 -0600

    protect alternative environments with htttp auth

--- web/app_test.php
+++ web/app_test.php
@@ -1,7 +1,6 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
@@ -16,11 +15,9 @@ if (!isset($_SERVER["PHP_AUTH_USER"]))
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-Debug::enable();
-
 require_once __DIR__.'/../app/AppKernel.php';
 
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('test', true);
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
