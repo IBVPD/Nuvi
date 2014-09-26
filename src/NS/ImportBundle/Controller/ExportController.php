d9ecc8ead448377267046bf0eb86cb131beb8284
Nathanael d. Noblet <nathanael@noblet.ca>
2014-09-26 16:58:22 -0600

    export fields

--- src/NS/ImportBundle/Controller/ExportController.php
+++ src/NS/ImportBundle/Controller/ExportController.php
@@ -2,9 +2,12 @@
 
 namespace NS\ImportBundle\Controller;
 
-use \Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \Exporter\Source\DoctrineORMQuerySourceIterator;
+use \NS\SentinelBundle\Entity\IBD;
+use \NS\SentinelBundle\Entity\RotaVirus;
 use \Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use \Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use \Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use \Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -21,27 +24,28 @@ class ExportController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $format  = 'xls';
+        $alias   = 'i';
+
         $ibdForm = $this->createForm('IBDReportFilterType',null,array('validation_groups'=> array('FieldPopulation'),'include_filter'=>false));
         $ibdForm->handleRequest($request);
         if($ibdForm->isValid())
         {
-            $alias  = 'i';
-            $fields = array('id','site.name','country.name','region.name');
+            $obj    = new IBD();
+            $fields = array_merge(array('id','site.name','country.name','region.name'),$obj->getMinimumRequiredFields());
             $query  = $this->get('ns.model_manager')->getRepository('NSSentinelBundle:IBD')->exportQuery($alias);
 
-            return $this->export($format, $ibdForm, $query, $fields);
+            return $this->export( 'xls', $ibdForm, $query, $fields );
         }
 
         $rotaForm = $this->createForm('RotaVirusReportFilterType',null,array('validation_groups'=> array('FieldPopulation'),'include_filter'=>false));
         $rotaForm->handleRequest($request);
         if($rotaForm->isValid())
         {
-            $alias  = 'i';
-            $fields = array('id','site.name','country.name','region.name');
+            $obj    = new RotaVirus();
+            $fields = array_merge(array('id','site.name','country.name','region.name'),$obj->getMinimumRequiredFields());
             $query  = $this->get('ns.model_manager')->getRepository('NSSentinelBundle:RotaVirus')->exportQuery($alias);
 
-            return $this->export($format, $ibdForm, $query, $fields);
+            return $this->export( 'xls', $rotaForm, $query, $fields );
         }
 
         return array( 'ibdForm' => $ibdForm->createView(), 'rotaForm'=>$rotaForm->createView() );
@@ -50,7 +54,7 @@ class ExportController extends Controller
     public function export($format, $form, $query, $fields)
     {
         $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($form, $query, $query->getRootAlias());
-        $source   = new \Exporter\Source\DoctrineORMQuerySourceIterator($query->getQuery(),$fields);
+        $source   = new DoctrineORMQuerySourceIterator($query->getQuery(),$fields);
         $filename = sprintf('export_%s.%s',date('Y_m_d_H_i_s'), $format);
 
         return $this->get('sonata.admin.exporter')->getResponse($format, $filename, $source);
