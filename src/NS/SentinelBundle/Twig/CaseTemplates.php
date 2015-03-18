4b12273bec3e0932383d9a7dd917fba1fad933d9
Nathanael d. Noblet <nathanael@noblet.ca>
2015-03-18 13:14:34 -0600

    fix listing for regional users

--- src/NS/SentinelBundle/Twig/CaseTemplates.php
+++ src/NS/SentinelBundle/Twig/CaseTemplates.php
@@ -14,12 +14,21 @@ class CaseTemplates extends \Twig_Extension
     private $securityContext;
     private $twig;
 
-    public function __construct(SecurityContextInterface $context, \Twig_Environment $environment)
+    /**
+     *
+     * @param SecurityContextInterface $context
+     * @param \Twig_Environment $twig
+     */
+    public function __construct(SecurityContextInterface $context, \Twig_Environment $twig)
     {
         $this->securityContext = $context;
-        $this->twig            = $environment;
+        $this->twig            = $twig;
     }
 
+    /**
+     *
+     * @return array
+     */
     public function getFunctions()
     {
         $isSafe = array('is_safe' => array('html'));
@@ -29,18 +38,33 @@ class CaseTemplates extends \Twig_Extension
         );
     }
 
+    /**
+     *
+     * @param array $results
+     * @param string $tableId
+     * @return string
+     */
     public function renderTable($results, $tableId)
     {
         $params = array('results' => $results, 'tableId' => $tableId);
 
         if ($this->securityContext->isGranted('ROLE_SITE_LEVEL'))
+        {
             return $this->twig->render('NSSentinelBundle:Case:site.html.twig', $params);
+        }
         else if ($this->securityContext->isGranted('ROLE_COUNTRY_LEVEL'))
+        {
             return $this->twig->render('NSSentinelBundle:Case:country.html.twig', $params);
+        }
         else if ($this->securityContext->isGranted('ROLE_REGION_LEVEL'))
+        {
             return $this->twig->render('NSSentinelBundle:Case:region.html.twig', $params);
+        }
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return 'twig_case_templates';
