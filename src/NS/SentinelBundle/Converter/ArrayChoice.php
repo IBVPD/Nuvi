29d8b959687f665cd60766086fb3763105508abc
Nathanael d. Noblet <nathanael@noblet.ca>
2015-03-21 20:04:34 -0600

    simplified exception messages

--- src/NS/SentinelBundle/Converter/ArrayChoice.php
+++ src/NS/SentinelBundle/Converter/ArrayChoice.php
@@ -47,7 +47,7 @@ class ArrayChoice implements NamedValueConverterInterface
         }
         catch (UnexpectedValueException2 $ex)
         {
-            throw new UnexpectedValueException(sprintf("Unable to convert value '%s' for %s",$input,join('', array_slice(explode('\\', $this->class), -1))), null, $ex);
+            throw new UnexpectedValueException(sprintf("Unable to convert value '%s' for %s",$input,$this->name), null, $ex);
         }
     }
 
--- src/NS/SentinelBundle/Tests/Converter/ArrayChoiceTest.php
+++ src/NS/SentinelBundle/Tests/Converter/ArrayChoiceTest.php
@@ -2,6 +2,55 @@
 
 namespace NS\SentinelBundle\Tests\Converter;
 
+use Ddeboer\DataImport\Exception\UnexpectedValueException;
+use NS\SentinelBundle\Converter\ArrayChoice;
+use NS\SentinelBundle\Form\Types\AlternateTripleChoice;
+use NS\SentinelBundle\Form\Types\BinaxResult;
+use NS\SentinelBundle\Form\Types\CaseStatus;
+use NS\SentinelBundle\Form\Types\CreateRoles;
+use NS\SentinelBundle\Form\Types\CSFAppearance;
+use NS\SentinelBundle\Form\Types\CultureResult;
+use NS\SentinelBundle\Form\Types\CXRResult;
+use NS\SentinelBundle\Form\Types\Dehydration;
+use NS\SentinelBundle\Form\Types\Diagnosis;
+use NS\SentinelBundle\Form\Types\DischargeClassification;
+use NS\SentinelBundle\Form\Types\DischargeDiagnosis;
+use NS\SentinelBundle\Form\Types\DischargeOutcome;
+use NS\SentinelBundle\Form\Types\ElisaKit;
+use NS\SentinelBundle\Form\Types\ElisaResult;
+use NS\SentinelBundle\Form\Types\FourDoses;
+use NS\SentinelBundle\Form\Types\Gender;
+use NS\SentinelBundle\Form\Types\GenotypeResultG;
+use NS\SentinelBundle\Form\Types\GenotypeResultP;
+use NS\SentinelBundle\Form\Types\GramStain;
+use NS\SentinelBundle\Form\Types\GramStainOrganism;
+use NS\SentinelBundle\Form\Types\HiSerotype;
+use NS\SentinelBundle\Form\Types\IBDCaseResult;
+use NS\SentinelBundle\Form\Types\IBDIntenseSupport;
+use NS\SentinelBundle\Form\Types\IsolateType;
+use NS\SentinelBundle\Form\Types\LatResult;
+use NS\SentinelBundle\Form\Types\MeningitisVaccinationReceived;
+use NS\SentinelBundle\Form\Types\MeningitisVaccinationType;
+use NS\SentinelBundle\Form\Types\NmSerogroup;
+use NS\SentinelBundle\Form\Types\OtherSpecimen;
+use NS\SentinelBundle\Form\Types\PathogenIdentifier;
+use NS\SentinelBundle\Form\Types\PCRResult;
+use NS\SentinelBundle\Form\Types\PCVType;
+use NS\SentinelBundle\Form\Types\Rehydration;
+use NS\SentinelBundle\Form\Types\Role;
+use NS\SentinelBundle\Form\Types\RotavirusDischargeOutcome;
+use NS\SentinelBundle\Form\Types\RotavirusVaccinationReceived;
+use NS\SentinelBundle\Form\Types\RotavirusVaccinationType;
+use NS\SentinelBundle\Form\Types\SampleType;
+use NS\SentinelBundle\Form\Types\SerotypeIdentifier;
+use NS\SentinelBundle\Form\Types\SpnSerotype;
+use NS\SentinelBundle\Form\Types\SurveillanceConducted;
+use NS\SentinelBundle\Form\Types\ThreeDoses;
+use NS\SentinelBundle\Form\Types\TripleChoice;
+use NS\SentinelBundle\Form\Types\VaccinationReceived;
+use NS\SentinelBundle\Form\Types\Volume;
+use NS\UtilBundle\Form\Types\ArrayChoice as ArrayChoice2;
+
 /**
  * Description of ArrayChoiceTest
  *
@@ -9,82 +58,202 @@ namespace NS\SentinelBundle\Tests\Converter;
  */
 class ArrayChoiceTest extends \PHPUnit_Framework_TestCase
 {
-
     /**
      *
-     * @param ArrayChoice $obj
+     * @param ArrayChoice2 $obj
      * @dataProvider converterProvider
-     * @expectedException \Ddeboer\DataImport\Exception\UnexpectedValueException
+     * @expectedException UnexpectedValueException
      */
-    public function testArrayChoice($obj)
+    public function testArrayChoiceExceptions($obj,$name)
     {
-        $this->assertTrue(is_object($obj));
         $class     = get_class($obj);
-        $converter = new \NS\SentinelBundle\Converter\ArrayChoice($class);
+        $converter = new ArrayChoice($class);
+        $this->assertEquals($name, $converter->getName());
+        $obj->getValues();
+        $converter->convert(-12);
+    }
+    
+    /**
+     *
+     * @param ArrayChoice2 $obj
+     * @dataProvider converterProvider
+     */
+    public function testArrayChoice($obj,$name)
+    {
+        $class     = get_class($obj);
+        $converter = new ArrayChoice($class);
+        $this->assertEquals($name, $converter->getName());
         $values    = $obj->getValues();
 
         foreach (array_keys($values) as $key)
         {
             $convertedObj = $converter->convert($key);
-            $this->assertInstanceOf($class, $convertedObj, sprintf("Converted '%s' to object of class %s", $key, $class));
+            $this->assertInstanceOf($class, $convertedObj);
             $this->assertEquals($key, $convertedObj->getValue());
         }
 
-        $convertedObj = $converter->convert(98);
-        $this->assertInstanceOf($class, $convertedObj, sprintf("Converted '%s' to object of class %s", 98, $class));
-        $this->assertEquals(99, $convertedObj->getValue());
-
-        $convertedObj = $converter->convert(-12);
     }
 
     public function converterProvider()
     {
         return array(
-            array('obj' => new \NS\SentinelBundle\Form\Types\AlternateTripleChoice()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\BinaxResult()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\CSFAppearance()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\CXRResult()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\CaseStatus()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\CreateRoles()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\CultureResult()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\Dehydration()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\Diagnosis()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\DischargeClassification()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\DischargeDiagnosis()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\DischargeOutcome()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\ElisaKit()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\ElisaResult()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\FourDoses()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\Gender()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\GenotypeResultG()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\GenotypeResultP()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\GramStain()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\GramStainOrganism()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\HiSerotype()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\IBDCaseResult()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\IBDIntenseSupport()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\IsolateType()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\LatResult()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\MeningitisVaccinationReceived()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\MeningitisVaccinationType()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\NmSerogroup()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\OtherSpecimen()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\PCRResult()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\PCVType()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\PathogenIdentifier()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\Rehydration()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\Role()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\RotavirusDischargeOutcome()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\RotavirusVaccinationReceived()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\RotavirusVaccinationType()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\SampleType()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\SerotypeIdentifier()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\SpnSerotype()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\SurveillanceConducted()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\ThreeDoses()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\TripleChoice()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\VaccinationReceived()),
-            array('obj' => new \NS\SentinelBundle\Form\Types\Volume()),
-        );
+            array(
+                'obj' => new AlternateTripleChoice(),
+                'name'=>'AlternateTripleChoice'
+                ),
+            array(
+                'obj' => new BinaxResult(),
+                'name'=>'BinaxResult',
+                ),
+            array(
+                'obj' => new CSFAppearance(),
+                'name'=>'CSFAppearance',
+                ),
+            array(
+                'obj' => new CXRResult(),
+                'name'=>'CXRResult',
+                ),
+            array(
+                'obj' => new CaseStatus(),
+                'name'=>'CaseStatus',
+                ),
+            array(
+                'obj' => new CreateRoles(),
+                'name'=>'CreateRoles',
+                ),
+            array(
+                'obj' => new CultureResult(),
+                'name'=>'CultureResult',
+                ),
+            array(
+                'obj' => new Dehydration(),
+                'name'=>'Dehydration',
+                ),
+            array(
+                'obj' => new Diagnosis(),
+                'name'=>'Diagnosis',
+                ),
+            array(
+                'obj' => new DischargeClassification(),
+                'name'=>'DischargeClassification',
+                ),
+            array(
+                'obj' => new DischargeDiagnosis(),
+                'name'=>'DischargeDiagnosis',
+                ),
+            array(
+                'obj' => new DischargeOutcome(),
+                'name'=>'DischargeOutcome',
+                ),
+            array(
+                'obj' => new ElisaKit(),
+                'name'=>'ElisaKit',
+                ),
+            array(
+                'obj' => new ElisaResult(),
+                'name'=>'ElisaResult',
+                ),
+            array(
+                'obj' => new FourDoses(),
+                'name'=>'FourDoses',
+                ),
+            array(
+                'obj' => new Gender(),
+                'name'=>'Gender',
+                ),
+            array(
+                'obj' => new GenotypeResultG(),
+                'name'=>'GenotypeResultG',
+                ),
+            array(
+                'obj' => new GenotypeResultP(),
+                'name'=>'GenotypeResultP',
+                ),
+            array(
+                'obj' => new GramStain(),
+                'name'=>'GramStain',
+                ),
+            array(
+                'obj' => new GramStainOrganism(),
+                'name'=>'GramStainOrganism'),
+            array(
+                'obj' => new HiSerotype(),
+                'name'=>'HiSerotype'),
+            array(
+                'obj' => new IBDCaseResult(),
+                'name'=>'IBDCaseResult'),
+            array(
+                'obj' => new IBDIntenseSupport(),
+                'name'=>'IBDIntenseSupport'),
+            array(
+                'obj' => new IsolateType(),
+                'name'=>'IsolateType'),
+            array(
+                'obj' => new LatResult(),
+                'name'=>'LatResult'),
+            array(
+                'obj' => new MeningitisVaccinationReceived(),
+                'name' => 'MeningitisVaccinationReceived'),
+            array(
+                'obj' => new MeningitisVaccinationType(),
+                'name'=>'MeningitisVaccinationType'),
+            array(
+                'obj' => new NmSerogroup(),
+                'name'=>'NmSerogroup'),
+            array(
+                'obj' => new OtherSpecimen(),
+                'name'=>'OtherSpecimen'),
+            array(
+                'obj' => new PCRResult(),
+                'name'=>'PCRResult'),
+            array(
+                'obj' => new PCVType(),
+                'name'=>'PCVType'),
+            array(
+                'obj' => new PathogenIdentifier(),
+                'name'=>'PathogenIdentifier'),
+            array(
+                'obj' => new Rehydration(),
+                'name'=>'Rehydration'),
+            array(
+                'obj' => new Role(),
+                'name'=>'Role'),
+            array(
+                'obj' => new RotavirusDischargeOutcome(),
+                'name'=>'RotavirusDischargeOutcome'),
+            array(
+                'obj' => new RotavirusVaccinationReceived(),
+                'name'=>'RotavirusVaccinationReceived',
+                ),
+            array(
+                'obj' => new RotavirusVaccinationType(),
+                'name'=>'RotavirusVaccinationType'),
+            array(
+                'obj' => new SampleType(),
+                'name'=>'SampleType'),
+            array(
+                'obj' => new SerotypeIdentifier(),
+                'name'=>'SerotypeIdentifier'),
+            array(
+                'obj' => new SpnSerotype(),
+                'name'=> 'SpnSerotype'
+                ),
+            array(
+                'obj' => new SurveillanceConducted(),
+                'name'=>'SurveillanceConducted'),
+            array(
+                'obj' => new ThreeDoses(),
+                'name'=>'ThreeDoses'),
+            array(
+                'obj' => new TripleChoice(),
+                'name'=>'TripleChoice'),
+            array(
+                'obj' => new VaccinationReceived(),
+                'name'=>'VaccinationReceived'),
+            array(
+                'obj' => new Volume(),
+                'name'=>'Volume',
+                ),
+                );
     }
 }
