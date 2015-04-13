3edbae684a3bac13ef6cf01ded5e4c8bb1426dab
Mark Woynarski <mark.woynarski@nobletdesign.com>
2015-04-13 16:21:54 -0600

    Input or Select fields

--- src/NS/SentinelBundle/Resources/public/js/main.js
+++ src/NS/SentinelBundle/Resources/public/js/main.js
@@ -1,26 +1,20 @@
-//Prototype.Browser.IE6 = Prototype.Browser.IE && parseInt(navigator.userAgent.substring(navigator.userAgent.indexOf("MSIE")+5)) === 6;
-//Prototype.Browser.IE7 = Prototype.Browser.IE && parseInt(navigator.userAgent.substring(navigator.userAgent.indexOf("MSIE")+5)) === 7;
-//Prototype.Browser.IE8 = Prototype.Browser.IE && parseInt(navigator.userAgent.substring(navigator.userAgent.indexOf("MSIE")+5)) === 8;
-//
-//if(!Prototype.Browser.IE8 && !Prototype.Browser.IE7 && !Prototype.Browser.IE6)
-//{
-//    Event.observe(window, 'resize', function(event)
-//    {
-//        drawGoogleCharts();
-//    });
-//}
-
-//$(window).load(function(event)
-//{
-//    drawGoogleCharts();    
-//});
-//
-//Event.observe(document, 'hinclude:success', function(event)
-//{
-//    drawGoogleCharts();
-//});
-//
-//Event.observe(document, 'ajaxUpdateSuccess', function(event)
-//{
-//    drawGoogleCharts();    
-//});
+$(document).ready(function()
+{
+    $('select.inputOrSelect').each(function()
+    {
+        if($(this).val() !== '')
+            $(this).prev('input.inputOrSelect').hide();
+    });
+    
+    $('select.inputOrSelect').change(function()
+    {
+        if($(this).val() === '')
+        {
+            $(this).prev('input.inputOrSelect').show().focus();
+        }
+        else
+        {
+            $(this).prev('input.inputOrSelect').hide();
+        }
+    });
+});
