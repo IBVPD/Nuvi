d1cafcfb3d97087628d8b000de7e093cdd0fe261
Nathanael d. Noblet <nathanael@noblet.ca>
2015-04-15 16:55:20 -0600

    whitespace formatting

--- src/NS/SentinelBundle/Security/UserProvider.php
+++ src/NS/SentinelBundle/Security/UserProvider.php
@@ -2,12 +2,14 @@
 
 namespace NS\SentinelBundle\Security;
 
-use Symfony\Component\Security\Core\User\UserProviderInterface;
+use \Doctrine\Common\Persistence\ObjectManager;
+use \Doctrine\ORM\NonUniqueResultException;
+use \Doctrine\ORM\NoResultException;
+use \NS\SentinelBundle\Entity\User;
+use \Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use \Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use \Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
-use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-use Doctrine\Common\Persistence\ObjectManager;
-use NS\SentinelBundle\Entity\User;
+use \Symfony\Component\Security\Core\User\UserProviderInterface;
 
 /**
  * Description of UserProvider
@@ -17,45 +19,65 @@ use NS\SentinelBundle\Entity\User;
 class UserProvider implements UserProviderInterface
 {
     private $entityMgr;
-    
+
+    /**
+     *
+     * @param ObjectManager $entityMgr
+     */
     public function __construct(ObjectManager $entityMgr)
     {
         $this->entityMgr = $entityMgr;
     }
 
-    public function loadUserByUsername($username) 
+    /**
+     *
+     * @param string $username
+     * @return UserInterface
+     * @throws UsernameNotFoundException
+     */
+    public function loadUserByUsername($username)
     {
-        try 
-        {
+        try {
             $user = $this->entityMgr->createQuery("SELECT u,a,l FROM NS\SentinelBundle\Entity\User u LEFT JOIN u.acls a LEFT JOIN u.referenceLab l WHERE u.email = :username")
-                ->setParameter('username',$username)
-                             ->getSingleResult();
+                ->setParameter('username', $username)
+                ->getSingleResult();
 
-            if (null === $user)
+            if (null === $user) {
                 throw new UsernameNotFoundException(sprintf('User "%s" not found.', $username));
+            }
 
-            $user->setTTL(time()+3600);
+            $user->setTTL(time() + 3600);
             return $user;
         }
-        catch(Doctrine\ORM\NonUniqueResultException $e)
-        {
-            throw new UsernameNotFoundException(sprintf("Username %s is not unique",$username),0, $e);
+        catch (NonUniqueResultException $e) {
+            throw new UsernameNotFoundException(sprintf("Username %s is not unique", $username), 0, $e);
         }
-        catch(\Doctrine\ORM\NoResultException $e)
-        {
+        catch (NoResultException $e) {
             throw new UsernameNotFoundException("User not found", 0, $e);
         }
     }
 
-    public function refreshUser(UserInterface $user) 
+    /**
+     *
+     * @param UserInterface $user
+     * @return UserInterface
+     * @throws UnsupportedUserException
+     */
+    public function refreshUser(UserInterface $user)
     {
-        if (!$user instanceof User) 
+        if (!$user instanceof User) {
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        }
 
-        return (time() > $user->getTTL()) ? $this->loadUserByUsername($user->getUsername()): $user;
+        return (time() > $user->getTTL()) ? $this->loadUserByUsername($user->getUsername()) : $user;
     }
 
-    public function supportsClass($class) 
+    /**
+     *
+     * @param string $class
+     * @return boolean
+     */
+    public function supportsClass($class)
     {
         return $class == 'NS\SentinelBundle\Entity\User';
     }
