ba0ce3e7c482112fd610fd33bf8fd765c9f787c5
Nathanael d. Noblet <nathanael@noblet.ca>
2015-07-21 09:53:20 -0600

    whitespace/formatting + docblock

--- src/NS/SentinelBundle/Menu/Builder.php
+++ src/NS/SentinelBundle/Menu/Builder.php
@@ -24,54 +24,56 @@ class Builder
         $this->factory         = $factory;
         $this->securityContext = $securityContext;
     }
-    
+
+    /**
+     * @return \Knp\Menu\ItemInterface
+     */
     public function sidebar()
     {
         $menu = $this->factory->createItem('root');
 
         $menu->setChildrenAttribute('class','nav nav-list');
-        if( $this->securityContext->isGranted('IS_AUTHENTICATED_FULLY') )
-        {
-            if($this->securityContext->isGranted('ROLE_CAN_CREATE'))
-            {
+        if ($this->securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
+            if ($this->securityContext->isGranted('ROLE_CAN_CREATE')) {
                 $data = $menu->addChild('Data Entry', array('label'=> 'menu.data-entry'))->setExtra('icon','fa fa-edit');
                 $data->addChild('Meningitis', array('label' => 'menu.ibd', 'route' => 'ibdIndex'));
                 $data->addChild('Rotavirus', array('route'=>'rotavirusIndex'))->setExtra('translation_domain', 'NSSentinelBundle');
             }
 
-            $reports = $menu->addChild('Reports', array('label'=> 'menu.data-reports'))->setExtra('icon','fa fa-dashboard');
+            $reports   = $menu->addChild('Reports', array('label' => 'menu.data-reports'))->setExtra('icon', 'fa fa-dashboard');
             $ibdReport = $reports->addChild('IBD');
-            $rotaReport = $reports->addChild('Rota');
+            $reports->addChild('Rota');
+
             $ibdReport->addChild('Age Distribution',array('label'=> 'menu.data-reports-age-distribution','route'=>'reportAnnualAgeDistribution'));
             $ibdReport->addChild('Enrolment %',array('label'=> 'menu.data-reports-percent-enrolled','route'=>'reportPercentEnrolled'));
             $ibdReport->addChild('Field Population',array('label'=>'menu.data-reports-field-population','route'=>'reportFieldPopulation'));
             $ibdReport->addChild('Culture Positive',array('label'=>'menu.data-reports-culture-positive','route'=>'reportCulturePositive'));
 
-            if($this->securityContext->isGranted('ROLE_API'))
-            {
-                $api = $menu->addChild('Api Resources',array('label'=>'Api Resources'))->setExtra('icon','fa fa-book');
-                $api->addChild('Dashboard',array('label'=>'Dashboard','route'=>'ns_api_dashboard'));
-                $api->addChild('Documentation',array('label'=>'Documentation','route'=>'nelmio_api_doc_index'));
+            if ($this->securityContext->isGranted('ROLE_API')) {
+                $api = $menu->addChild('Api Resources', array('label' => 'Api Resources'))->setExtra('icon', 'fa fa-book');
+                $api->addChild('Dashboard', array('label' => 'Dashboard', 'route' => 'ns_api_dashboard'));
+                $api->addChild('Documentation', array('label' => 'Documentation','route' => 'nelmio_api_doc_index'));
             }
 
-            if($this->securityContext->isGranted('ROLE_IMPORT'))
-            {
-                $import = $menu->addChild('Import',array('label'=>'menu.import-export'))->setExtra('icon', 'fa fa-cloud-upload');
-                $import->addChild('Import',array('label'=>'menu.import','route'=>'importIndex'))->setExtra('icon', 'fa fa-cloud-upload');
-                $import->addChild('Export',array('label'=>'menu.export','route'=>'exportIndex'))->setExtra('icon', 'fa fa-cloud-download');
+            if ($this->securityContext->isGranted('ROLE_IMPORT')) {
+                $import = $menu->addChild('Import', array('label' => 'menu.import-export'))->setExtra('icon', 'fa fa-cloud-upload');
+                $import->addChild('Import', array('label' => 'menu.import', 'route' => 'importIndex'))->setExtra('icon', 'fa fa-cloud-upload');
+                $import->addChild('Export', array('label' => 'menu.export', 'route' => 'exportIndex'))->setExtra('icon', 'fa fa-cloud-download');
             }
 
-            if($this->securityContext->isGranted('ROLE_ADMIN'))
-            {
-                $admin = $menu->addChild('Admin', array('label'=> 'menu.data-admin'))->setExtra('icon','fa fa-desktop');
-                $admin->addChild('Admin', array('label'=> 'menu.data-admin','route'=>'sonata_admin_dashboard'));
-                $admin->addChild('Translation',array('label'=> 'menu.translation','route'=>'jms_translation_index'));
+            if ($this->securityContext->isGranted('ROLE_ADMIN')) {
+                $admin = $menu->addChild('Admin', array('label' => 'menu.data-admin'))->setExtra('icon', 'fa fa-desktop');
+                $admin->addChild('Admin', array('label' => 'menu.data-admin', 'route' => 'sonata_admin_dashboard'));
+                $admin->addChild('Translation', array('label' => 'menu.translation','route' => 'jms_translation_index'));
             }
         }
 
         return $menu;
     }
-    
+
+    /**
+     * @return \Knp\Menu\ItemInterface
+     */
     public function user()
     {
         $menu = $this->factory->createItem('root');
