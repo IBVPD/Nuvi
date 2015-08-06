fae8bc863e7b39f26d13b0bd59cb75440c2b2ee9
Nathanael d. Noblet <nathanael@noblet.ca>
2015-08-05 23:10:49 -0600

    custom file directory namer

---
+++ src/NS/ImportBundle/Vich/DirectoryNamer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gnat
+ * Date: 05/08/15
+ * Time: 3:21 PM
+ */
+
+namespace NS\ImportBundle\Vich;
+
+use \Vich\UploaderBundle\Mapping\PropertyMapping;
+use \Vich\UploaderBundle\Naming\DirectoryNamerInterface;
+
+class DirectoryNamer implements DirectoryNamerInterface
+{
+    /**
+     * Creates a directory name for the file being uploaded.
+     *
+     * @param object $object The object the upload is attached to.
+     * @param PropertyMapping $mapping The mapping to use to manipulate the given object.
+     *
+     * @return string The directory name.
+     */
+    public function directoryName($object, PropertyMapping $mapping)
+    {
+        return sha1($object->getMapName().$object->getCreatedAt()->format('Y-m-d H:i:s').$object->getUser()->__toString());
+    }
+}
