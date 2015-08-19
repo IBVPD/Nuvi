f5b54d60335eb022991578790f332f73240c1f9c
Nathanael d. Noblet <nathanael@noblet.ca>
2015-08-19 15:56:15 -0600

    ensure state is kept between runs

--- src/NS/ImportBundle/Tests/Filter/DuplicateTest.php
+++ src/NS/ImportBundle/Tests/Filter/DuplicateTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace NS\ImportBundle\Tests\Filter;
+use NS\ImportBundle\Filter\Duplicate;
 use Symfony\Component\HttpFoundation\File\File;
 
 /**
@@ -27,6 +28,7 @@ class DuplicateTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($duplicate->__invoke($paramsTwo), "Second set of params is a duplicate");
         $this->assertTrue($duplicate->hasMessage(), "There is a message");
         $this->assertEquals('Duplicate row detected with key \'sitecode_12223\'',$duplicate->getMessage());
+        unlink($file->getPathname());
     }
 
     public function testDuplicateTextOnlyItemIsDetected()
@@ -39,6 +41,7 @@ class DuplicateTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($duplicate->hasMessage(), "There is no message");
         $this->assertFalse($duplicate->__invoke($params), "Second set of params is a duplicate");
         $this->assertTrue($duplicate->hasMessage(), "There is a message");
+        unlink($file->getPathname());
     }
 
     public function testDuplicateObjectItemIsDetected()
@@ -60,5 +63,33 @@ class DuplicateTest extends \PHPUnit_Framework_TestCase
         $this->assertCount(1, $duplicateArray);
         $this->assertArrayHasKey(0, $duplicateArray);
         $this->assertEquals('my-code_12223', $duplicateArray[0], "The duplicate array has the proper duplicate key");
+        unlink($file->getPathname());
+    }
+
+    public function testDuplicateObjectStoresState()
+    {
+        $file = new File(tempnam(sys_get_temp_dir(),'duptest'));
+        $uniqueFields = array('code','caseId');
+
+        $params = array(
+            array('code'=>1,'caseId'=>2,),
+            array('code'=>2,'caseId'=>3,),
+            array('code'=>2,'caseId'=>2,),
+            array('code'=>3,'caseId'=>1,),
+        );
+        $duplicateOne = new Duplicate($uniqueFields,$file);
+        foreach($params as $param) {
+            $this->assertTrue($duplicateOne->__invoke($param));
+        }
+        $duplicateOne->finish();
+
+        $this->assertNotNull(file_get_contents($file->getPathname()));
+
+        $parameter = array('code'=>2,'caseId'=>3);
+        $duplicateTwo = new Duplicate($uniqueFields,$file);
+        $this->assertFalse($duplicateTwo->__invoke($parameter));
+        $this->assertTrue($duplicateTwo->hasMessage());
+
+        unlink($file->getPathname());
     }
 }
