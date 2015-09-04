27c5792b225f92696b8c4be9d33bb5dc85c4c626
Nathanael d. Noblet <nathanael@noblet.ca>
2015-09-04 15:17:30 -0600

    added a trimmer

---
+++ src/NS/ImportBundle/Converter/TrimInputConverter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace NS\ImportBundle\Converter;
+
+use Ddeboer\DataImport\Step\ConverterStep;
+
+/**
+ * Class TrimInputConverter
+ * @package NS\ImportBundle\Converter
+ */
+class TrimInputConverter extends ConverterStep
+{
+    /**
+     * @param $item
+     * @return mixed
+     */
+    public function __invoke($item)
+    {
+        return $this->trim($item);
+    }
+
+    /**
+     * @param $item
+     * @return mixed
+     */
+    public function trim($item){
+        foreach($item as &$value) {
+            if(is_array($value)){
+                $value = $this->trim($value);
+            } elseif( !is_object($value) ) {
+                $value = trim($value);
+            }
+        }
+
+        return $item;
+    }
+}
---
+++ src/NS/ImportBundle/Tests/Converter/TrimInputConverterTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace NS\ImportBundle\Tests\Converter;
+
+use NS\ImportBundle\Converter\TrimInputConverter;
+
+class TrimInputConverterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param array $inputData
+     * @param array $outputData
+     * @dataProvider getData
+     */
+    public function testTrimInput(array $inputData, array $outputData)
+    {
+        $converter = new TrimInputConverter();
+        $retData = call_user_func($converter,$inputData);
+        $this->assertEquals($outputData,$retData);
+    }
+
+    public function getData()
+    {
+        $date = new \DateTime();
+        $stdClass = new \stdClass();
+
+        return array(
+            array(
+                'inputData' => array(),
+                'outputData' => array(),
+            ),
+            array(
+                'inputData' => array(
+                    'field1' => '  0',
+                    'field2' => ' 1 ',
+                    'subarray' => array(
+                        'field1' => 1,
+                        'field2' => $date,
+                        'field3'=> '  '.'1',
+                    ),
+                    'field3'=> $stdClass
+                ),
+                'outputData' => array(
+                    'field1' => '0',
+                    'field2' => '1',
+                    'subarray' => array(
+                        'field1' => 1,
+                        'field2' => $date,
+                        'field3'=> '1',
+                    ),
+                    'field3'=> $stdClass
+                ),
+            ),
+        );
+    }
+}
