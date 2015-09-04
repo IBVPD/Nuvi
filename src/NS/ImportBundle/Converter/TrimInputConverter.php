6c02d3222e9dfb9c5f7635ceb301bd2394c532e6
Nathanael d. Noblet <nathanael@noblet.ca>
2015-09-04 16:04:46 -0600

    whitespace/formatting

--- src/NS/ImportBundle/Converter/TrimInputConverter.php
+++ src/NS/ImportBundle/Converter/TrimInputConverter.php
@@ -23,11 +23,12 @@ class TrimInputConverter extends ConverterStep
      * @param $item
      * @return mixed
      */
-    public function trim($item){
-        foreach($item as &$value) {
-            if(is_array($value)){
+    public function trim($item)
+    {
+        foreach ($item as &$value) {
+            if (is_array($value)) {
                 $value = $this->trim($value);
-            } elseif( !is_object($value) ) {
+            } elseif (!is_object($value)) {
                 $value = trim($value);
             }
         }
