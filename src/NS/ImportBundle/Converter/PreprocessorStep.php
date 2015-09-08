1609758242277762586c584390db6b477129c711
Nathanael d. Noblet <nathanael@noblet.ca>
2015-09-08 12:15:10 -0600

    preprocessor step

---
+++ src/NS/ImportBundle/Converter/PreprocessorStep.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace NS\ImportBundle\Converter;
+
+use Ddeboer\DataImport\Report;
+use Ddeboer\DataImport\Step;
+use NS\ImportBundle\Converter\Expression\ExpressionBuilder;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+/**
+ * Class ExpressionConverter
+ * @package NS\ImportBundle\Converter
+ */
+class PreprocessorStep implements Step
+{
+    /**
+     * @var ExpressionLanguage
+     */
+    private $language;
+
+    /**
+     * @var ExpressionBuilder
+     */
+    private $expressionBuilder;
+
+    /**
+     * @var array
+     */
+    private $conditions = array();
+
+    /**
+     * ExpressionConverter constructor.
+     * @param array $conditions
+     */
+    public function __construct(ExpressionBuilder $builder)
+    {
+        $this->expressionBuilder = $builder;
+        $this->language = new ExpressionLanguage();
+    }
+
+    /**
+     * @param callable $property
+     * @param array $conditions
+     * @return $this|void
+     */
+    public function add($property, $conditions)
+    {
+        $this->conditions[$property] = (is_array($conditions)) ? $conditions : array($conditions);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(&$item, Report $report = null)
+    {
+        $accessor = new PropertyAccessor();
+
+        foreach ($this->conditions as $property => $conditions) {
+            foreach ($conditions as $condition) {
+                $expr = $this->expressionBuilder->convertRuleToExpression($condition->getRule());
+                if($this->language->evaluate($expr, array('item'=>$item))) {
+                    $accessor->setValue($item, sprintf('[%s]',$property), $condition->getValue());
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/NS/ImportBundle/Tests/Converter/PreprocessorStepTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace NS\ImportBundle\Tests\Converter;
+
+
+use NS\ImportBundle\Converter\Expression\Condition;
+use NS\ImportBundle\Converter\Expression\ExpressionBuilder;
+use NS\ImportBundle\Converter\PreprocessorStep;
+
+class PreprocessorStepTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getCondition
+     */
+    public function testConverter(array $condParam, $value, array $item)
+    {
+        $condition = new Condition($condParam, $value);
+
+        $step = new PreprocessorStep(new ExpressionBuilder());
+        $step->add('fieldOne',$condition);
+        $step->process($item);
+        $this->assertEquals($value, $item['fieldOne']);
+    }
+
+    public function getCondition()
+    {
+        return array(
+            array(
+                'cond' => array(
+                    'condition' => 'OR',
+                    'rules' => array(
+                        array('field' => 'fieldOne', 'operator' => 'equal', 'value' => '2'),
+                        array('field' => 'fieldOne', 'operator' => 'equal', 'value' => '3'),
+                    ),),
+                'output' => 1,
+                'item' => array('fieldOne'=>2,'fieldTwo'=>2)
+            ),
+        );
+    }
+}
