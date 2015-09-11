e35a014f463ad4441e3d871a8810ae96ebca01eb
Nathanael d. Noblet <nathanael@noblet.ca>
2015-09-11 15:13:10 -0600

    Convert CSV Files to UTF-8 on upload
    Fixes #6

---
+++ src/NS/ImportBundle/Tests/Vich/EventListenerTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace NS\ImportBundle\Tests\Vich;
+
+use NS\ImportBundle\Entity\Import;
+use NS\ImportBundle\Vich\EventListener;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Event\Event;
+use Vich\UploaderBundle\Mapping\PropertyMapping;
+
+class EventListenerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNonImport()
+    {
+        $listener = $this->getMockBuilder('NS\ImportBundle\Vich\EventListener')
+            ->setMethods(array('onPostUpload','convertToUtf8'))
+            ->getMock();
+
+        $listener->expects($this->never())->method('convertToUtf8');
+
+        $stdObj = new \stdClass();
+
+        $listener->onPostUpload(new Event($stdObj, new PropertyMapping('source','source')));
+    }
+
+    public function testNonSourceProperty()
+    {
+        $file = new File(__DIR__ . '/../Fixtures/EMR-IBD-headers.csv');
+        $listener = $this->getMockBuilder('NS\ImportBundle\Vich\EventListener')
+            ->setMethods(array('onPostUpload','convertToUtf8'))
+            ->getMock();
+
+        $listener->expects($this->never())->method('convertToUtf8');
+
+        $mockUser = $this->getMock('Symfony\Component\Security\Core\User\UserInterface');
+        $import = new Import($mockUser);
+        $import->setSourceFile($file);
+
+        $listener->onPostUpload(new Event($import, new PropertyMapping('messages','messages')));
+    }
+
+    public function testNonCsvFile()
+    {
+        $file = new File(__DIR__ . '/../Fixtures/EMR-IBD-headers.xls');
+        $listener = $this->getMockBuilder('NS\ImportBundle\Vich\EventListener')
+            ->setMethods(array('onPostUpload','convertToUtf8'))
+            ->getMock();
+
+        $listener->expects($this->never())->method('convertToUtf8');
+
+
+        $mockUser = $this->getMock('Symfony\Component\Security\Core\User\UserInterface');
+        $import = new Import($mockUser);
+        $import->setSourceFile($file);
+
+        $listener->onPostUpload(new Event($import, new PropertyMapping('source','source')));
+    }
+
+    public function testCsvFile()
+    {
+        $filePath = realpath(__DIR__ . '/../Fixtures/EMR-IBD-headers-utf16.csv');
+        $newFile = '/tmp/headers.csv';
+        if(!copy($filePath,$newFile)) {
+            $this->fail(sprintf('Unable to copy(%s,%s)',$filePath,$newFile));
+        }
+
+        $orgEncoding = mb_detect_encoding(file_get_contents($newFile));
+        $this->assertTrue($orgEncoding != 'UTF-8');
+
+        $file = new File($newFile);
+
+        $listener = new EventListener();
+
+        $mockUser = $this->getMock('Symfony\Component\Security\Core\User\UserInterface');
+        $import = new Import($mockUser);
+        $import->setSourceFile($file);
+
+        $listener->onPostUpload(new Event($import, new PropertyMapping('source','source')));
+        $newEncoding = mb_detect_encoding(file_get_contents($newFile));
+        $this->assertEquals('UTF-8',$newEncoding);
+        unlink($newFile);
+    }
+}
---
+++ src/NS/ImportBundle/Vich/EventListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace NS\ImportBundle\Vich;
+
+use NS\ImportBundle\Entity\Import;
+use NS\ImportBundle\Importer\ReaderFactory;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Event\Event;
+
+
+/**
+ * Class EventListener
+ * @package NS\ImportBundle\Vich
+ */
+class EventListener
+{
+    /**
+     * @param Event $event
+     */
+    public function onPostUpload(Event $event)
+    {
+        $obj      = $event->getObject();
+        $property = $event->getMapping()->getFilePropertyName();
+
+        if($obj instanceof Import && $property == 'source' && ReaderFactory::getExtension($obj->getSourceFile()) == 'csv') {
+            $this->convertToUtf8($obj->getSourceFile()->getPathname());
+        }
+    }
+
+    /**
+     * @param $path
+     */
+    public function convertToUtf8($path)
+    {
+        $fileContent = file_get_contents($path);
+        file_put_contents($path,mb_convert_encoding($fileContent,'UTF-8'));
+    }
+}
