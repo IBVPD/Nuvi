2fce3a1ef7f430cf6d140cb1bcb965134e04e92f
Nathanael d. Noblet <nathanael@noblet.ca>
2015-09-15 04:50:58 -0600

    whitespace formatting

--- src/NS/SentinelBundle/Form/IBD/BaseLabType.php
+++ src/NS/SentinelBundle/Form/IBD/BaseLabType.php
@@ -19,24 +19,30 @@ class BaseLabType extends AbstractType
     {
         $builder
             ->add('labId', null, array('label' => 'ibd-rrl-form.lab-id', 'required' => true))
-            ->add('sampleType', 'SampleType', array('label'    => 'ibd-rrl-form.sample-type',
-                'required' => false))
-            ->add('dateReceived', 'acedatepicker', array('label'    => 'ibd-rrl-form.date-received',
-                'required' => false))
-            ->add('isolateViable', 'IsolateViable', array('label'    => 'ibd-rrl-form.isolate-viable',
-                'required' => false))
-            ->add('isolateType', 'IsolateType', array('label'    => 'ibd-rrl-form.isolate-type',
-                'required' => false))
-            ->add('pathogenIdentifierMethod', 'PathogenIdentifier', array('label'    => 'ibd-rrl-form.pathogen-id-method',
-                'required' => false, 'attr' => array('data-context-child' => 'pathogenIdentifierMethod')))
-            ->add('pathogenIdentifierOther', null, array('label'    => 'ibd-rrl-form.pathogen-id-other',
-                'required' => false, 'attr' => array('data-context-parent' => 'pathogenIdentifierMethod',
-                    'data-context-value' => PathogenIdentifier::OTHER)))
-            ->add('serotypeIdentifier', 'SerotypeIdentifier', array('label'    => 'ibd-rrl-form.serotype-id-method',
-                'required' => false, 'attr' => array('data-context-child' => 'serotypeIdentifier')))
-            ->add('serotypeIdentifierOther', null, array('label'    => 'ibd-rrl-form.serotype-id-other',
-                'required' => false, 'attr' => array('data-context-parent' => 'serotypeIdentifier',
-                    'data-context-value' => SerotypeIdentifier::OTHER)))
+            ->add('sampleType', 'SampleType', array('label' => 'ibd-rrl-form.sample-type', 'required' => false))
+            ->add('dateReceived', 'acedatepicker', array('label' => 'ibd-rrl-form.date-received', 'required' => false))
+            ->add('isolateViable', 'IsolateViable', array('label' => 'ibd-rrl-form.isolate-viable', 'required' => false))
+            ->add('isolateType', 'IsolateType', array('label' => 'ibd-rrl-form.isolate-type', 'required' => false))
+            ->add('pathogenIdentifierMethod', 'PathogenIdentifier', array(
+                    'label' => 'ibd-rrl-form.pathogen-id-method',
+                    'required' => false,
+                    'attr' => array('data-context-child' => 'pathogenIdentifierMethod'))
+            )
+            ->add('pathogenIdentifierOther', null, array(
+                    'label' => 'ibd-rrl-form.pathogen-id-other',
+                    'required' => false, 'attr' => array('data-context-parent' => 'pathogenIdentifierMethod',
+                    'data-context-value' => PathogenIdentifier::OTHER))
+            )
+            ->add('serotypeIdentifier', 'SerotypeIdentifier', array(
+                    'label' => 'ibd-rrl-form.serotype-id-method',
+                    'required' => false,
+                    'attr' => array('data-context-child' => 'serotypeIdentifier'))
+            )
+            ->add('serotypeIdentifierOther', null, array(
+                    'label' => 'ibd-rrl-form.serotype-id-other',
+                    'required' => false, 'attr' => array('data-context-parent' => 'serotypeIdentifier',
+                    'data-context-value' => SerotypeIdentifier::OTHER))
+            )
             ->add('lytA', new CTValueType(), array('label' => 'ibd-rrl-form.lytA', 'required' => false))
             ->add('ctrA', new CTValueType(), array('label' => 'ibd-rrl-form.ctrA', 'required' => false))
             ->add('sodC', new CTValueType(), array('label' => 'ibd-rrl-form.sodC', 'required' => false))
@@ -44,15 +50,11 @@ class BaseLabType extends AbstractType
             ->add('hpd3', new CTValueType(), array('label' => 'ibd-rrl-form.hpd3', 'required' => false))
             ->add('bexA', new CTValueType(), array('label' => 'ibd-rrl-form.bexA', 'required' => false))
             ->add('rNaseP', new CTValueType(), array('label' => 'ibd-rrl-form.rNasP', 'required' => false))
-            ->add('finalResult', 'FinalResult', array('label' => 'ibd-rrl-form.finalResult', 'required' => false))
-            ->add('spnSerotype', 'SpnSerotype', array('label'    => 'ibd-rrl-form.spnSerotype',
-                'required' => false))
-            ->add('hiSerotype', 'HiSerotype', array('label'    => 'ibd-rrl-form.hiSerotype',
-                'required' => false))
-            ->add('nmSerogroup', 'NmSerogroup', array('label'    => 'ibd-rrl-form.nmSerogroup',
-                'required' => false))
-            ->add('comment',null,array('label'=>'ibd-rrl-form.comment','required'=>false))
-        ;
+            ->add('finalResult', 'FinalResult', array('label' => 'ibd-rrl-form.finalResult','required' => false))
+            ->add('spnSerotype', 'SpnSerotype', array('label' => 'ibd-rrl-form.spnSerotype', 'required' => false))
+            ->add('hiSerotype', 'HiSerotype', array('label' => 'ibd-rrl-form.hiSerotype', 'required' => false))
+            ->add('nmSerogroup', 'NmSerogroup', array('label' => 'ibd-rrl-form.nmSerogroup', 'required' => false))
+            ->add('comment', null, array('label' => 'ibd-rrl-form.comment', 'required' => false));
     }
 
     /**
