2d88b9184e569413aa7d7629fd58410a3024c0a8
Nathanael d. Noblet <nathanael@noblet.ca>
2015-10-15 22:20:33 -0600

    fix import result updater function method

--- src/NS/ImportBundle/Importer/ImportResultUpdater.php
+++ src/NS/ImportBundle/Importer/ImportResultUpdater.php
@@ -61,13 +61,13 @@ class ImportResultUpdater
     }
 
     /**
-     * @param \SplFileObject $reports
+     * @param \SplObjectStorage $reports
      * @param \SplFileObject $writer
-     * @param int            $severity
-     * @param boolean        $writeHeader
+     * @param int $severity
+     * @param boolean $writeHeader
      * @return int
      */
-    public function buildLog(\SplFileObject $reports, \SplFileObject $writer, $severity = null, $writeHeader = false)
+    public function buildLog(\SplObjectStorage $reports, \SplFileObject $writer, $severity = null, $writeHeader = false)
     {
         $rowCount = 0;
         $first = true;
@@ -96,7 +96,7 @@ class ImportResultUpdater
 
     /**
      * @param Import $import
-     * @param array|\SplObjectStorage $exceptions
+     * @param \SplObjectStorage $exceptions
      * @param boolean $writeHeader
      */
     public function buildExceptions(Import $import, \SplObjectStorage $exceptions, $writeHeader)
