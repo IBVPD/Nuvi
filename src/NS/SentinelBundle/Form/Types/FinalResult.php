7daf5ab6377b13ba840cf735f16501c108752da4
Nathanael d. Noblet <nathanael@noblet.ca>
2015-11-13 10:08:46 +0100

    added additional valid final result values

--- src/NS/SentinelBundle/Form/Types/FinalResult.php
+++ src/NS/SentinelBundle/Form/Types/FinalResult.php
@@ -16,12 +16,20 @@ class FinalResult extends ArrayChoice
     const NEG          = 4;
     const INCONCLUSIVE = 5;
     const NOT_DONE     = 6;
+    const SPN_HI       = 7;
+    const SPN_NM       = 8;
+    const HI_NM        = 9;
+    const SPN_HI_NM    = 10;
 
     protected $values = array(
         self::SPN          => 'Spn',
         self::HI           => 'Hi',
         self::NM           => 'Nm',
         self::NEG          => 'Neg',
+        self::SPN_HI       => 'Spn+Hi',
+        self::SPN_NM       => 'Spn+Nm',
+        self::HI_NM        => 'Hi+Nm',
+        self::SPN_HI_NM    => 'Spn+Hi+Nm',
         self::INCONCLUSIVE => 'Inconclusive',
         self::NOT_DONE     => 'Not Done',
     );
