0e5281acf9da7a891f8c603c564b744f7ee1b3c1
Nathanael d. Noblet <nathanael@noblet.ca>
2016-03-22 08:42:28 -0600

    Release of new version 0.3.11

---
+++ src/NS/SentinelBundle/Entity/Types/GramStainOrganism.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace NS\SentinelBundle\Entity\Types;
+use NS\UtilBundle\Entity\Types\ArrayChoice;
+
+class GramStainOrganism extends ArrayChoice
+{
+    protected $convert_class = 'NS\SentinelBundle\Form\Types\GramStainOrganism';
+
+    public function getName()
+    {
+        return 'GramStainOrganism';
+    }   
+}
+
