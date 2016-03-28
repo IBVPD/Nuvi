b3bf3963797de74f39ff521c412b25c2c7bd2495
Nathanael d. Noblet <nathanael@noblet.ca>
2016-03-28 16:00:48 -0600

    use json response

--- src/NS/ImportBundle/Controller/ConditionController.php
+++ src/NS/ImportBundle/Controller/ConditionController.php
@@ -3,30 +3,27 @@
 namespace NS\ImportBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use NS\ImportBundle\Converter\Expression\ConditionConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
 
 class ConditionController extends Controller
 {
     /**
      * @param Request $request
      * @Route("/ajax/conditions-as-expression",name="adminConditionAjaxConverter")
-     * @return string
+     * @return JsonResponse
      */
     public function conditionExpressionAction(Request $request)
     {
-        $preConditions  = json_decode($request->request->get('conditions'),true);
-
+        $preConditions = json_decode($request->request->get('conditions'), true);
         $string = null;
-        if(is_array($preConditions))
-        {
+        if (is_array($preConditions)) {
             $converter = new ConditionConverter();
-
             $string = $converter->toArray($preConditions);
         }
 
-        return new Response(json_encode($string));
+        return new JsonResponse(json_encode($string));
     }
 }
