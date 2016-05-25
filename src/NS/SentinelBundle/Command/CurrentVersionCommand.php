c1226a9dfbebeaa84f1224eabc38483a59dc1467
Nathanael d. Noblet <nathanael@noblet.ca>
2016-05-25 12:03:15 -0600

    added get version command

---
+++ src/NS/SentinelBundle/Command/CurrentVersionCommand.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gnat
+ * Date: 25/05/16
+ * Time: 11:58 AM
+ */
+
+namespace NS\SentinelBundle\Command;
+
+
+use NS\SentinelBundle\Version;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CurrentVersionCommand extends Command
+{
+    /**
+     * @inheritDoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('nssentinel:version')
+            ->setDescription('Get the current app version');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln(sprintf('Current Version: %s',Version::VERSION));
+    }
+}
