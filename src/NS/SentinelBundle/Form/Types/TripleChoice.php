ade8ebeea5173eba769b45d735c8236f12eaa91b
Nathanael d. Noblet <nathanael@noblet.ca>
2016-08-30 14:07:41 -0600

    allow for the unknown option to not be displayed

--- src/NS/SentinelBundle/Form/Types/TripleChoice.php
+++ src/NS/SentinelBundle/Form/Types/TripleChoice.php
@@ -4,6 +4,8 @@ namespace NS\SentinelBundle\Form\Types;
 
 use JMS\TranslationBundle\Translation\TranslationContainerInterface;
 use NS\UtilBundle\Form\Types\TranslatableArrayChoice;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Description of TripleChoice
@@ -20,4 +22,27 @@ class TripleChoice extends TranslatableArrayChoice implements TranslationContain
                             self::NO      => 'No',
                             self::YES     => 'Yes',
                             self::UNKNOWN => 'Unknown');
+
+    /**
+     * @inheritDoc
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $values = $this->values;
+
+        $resolver->setDefaults(array(
+            'empty_value' => 'Please Select...',
+            'exclude_unknown' => false,
+            'choices' => function (Options $options) use ($values) {
+                if ($options['exclude_unknown']) {
+                    unset($values[self::UNKNOWN]);
+                }
+
+                return $values;
+            }
+        ));
+
+        $resolver->setDefined(array('special_values'));
+        $resolver->addAllowedTypes(array('special_values' => 'array'));
+    }
 }
