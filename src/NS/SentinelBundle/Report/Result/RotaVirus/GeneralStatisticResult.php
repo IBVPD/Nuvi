415039876de6f5f1360e60c5c9ff0e34749e2020
Nathanael d. Noblet <nathanael@noblet.ca>
2017-02-13 10:18:49 -0700

    fix namespace of RotaDischargeOutcome

--- src/NS/SentinelBundle/Report/Result/RotaVirus/GeneralStatisticResult.php
+++ src/NS/SentinelBundle/Report/Result/RotaVirus/GeneralStatisticResult.php
@@ -8,19 +8,18 @@
 
 namespace NS\SentinelBundle\Report\Result\RotaVirus;
 
-
-use NS\SentinelBundle\Form\Types\RotavirusDischargeOutcome;
+use NS\SentinelBundle\Form\RotaVirus\Types\DischargeOutcome;
 use NS\SentinelBundle\Report\Result\AbstractGeneralStatisticResult;
 
 class GeneralStatisticResult extends AbstractGeneralStatisticResult
 {
     /**
      * @param array $dischargeOutcomeDistribution
-     * @return \NS\SentinelBundle\Report\Result\GeneralStatisticResult
+     * @return \NS\SentinelBundle\Report\Result\RotaVirus\GeneralStatisticResult
      */
     public function setDischargeOutcomeDistribution($dischargeOutcomeDistribution)
     {
-        $outcome = new RotavirusDischargeOutcome();
+        $outcome = new DischargeOutcome();
         foreach ($dischargeOutcomeDistribution as $result) {
             $outcome->setValue($result['outcome']);
             $this->dischargeOutcomeDistribution[$outcome->__toString()] = $result['caseCount'];
