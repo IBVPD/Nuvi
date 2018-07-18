5cfada032c476389847fc1b2053978485d031096
Nathanael d. Noblet <nathanael@noblet.ca>
2018-07-18 10:45:00 -0600

    Return 400 error message

--- src/NS/SentinelBundle/Controller/UserAdminController.php
+++ src/NS/SentinelBundle/Controller/UserAdminController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use NS\SentinelBundle\Form\Types\Role;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernel;
 
 class UserAdminController extends Controller
 {
@@ -19,6 +21,10 @@ class UserAdminController extends Controller
     {
         $vars = $request->request->all();
         $secondaryTypeValue = $vars['secondary-field'];
+        if (empty($secondaryTypeValue)) {
+            return new Response("Please select the type first", 400, ['Autocomplete-Error' => 'The Access Level is required']);
+        }
+
         $role = new Role($secondaryTypeValue);
 
         return $this->get('ns.ajax_autocompleter')->getAutocomplete($role->getClassMatch(), 'name');
