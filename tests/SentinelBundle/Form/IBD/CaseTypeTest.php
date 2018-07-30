f78887f21c521f328508fea31e5ab31ad61f1d28
Nathanael d. Noblet <nathanael@noblet.ca>
2018-07-30 14:30:33 -0600

    fix unmatched tests

--- tests/SentinelBundle/Form/IBD/CaseTypeTest.php
+++ tests/SentinelBundle/Form/IBD/CaseTypeTest.php
@@ -31,7 +31,7 @@ class CaseTypeTest extends TypeTestCase
 {
     public function testConstruction()
     {
-        $this->authChecker->expects($this->atLeast(2))->method('isGranted')->with('ROLE_AMR')->willReturn(false);
+        $this->authChecker->expects($this->atLeast(1))->method('isGranted')->with('ROLE_AMR')->willReturn(false);
         $form = $this->factory->create(CaseType::class);
         foreach ($form as $name => $field) {
             $this->assertEquals(in_array($name, $this->requiredFields), $field->getConfig()->getOption('required'), sprintf('Field %s doesn\'t have properly required field', $name));
@@ -45,7 +45,7 @@ class CaseTypeTest extends TypeTestCase
     {
         $fields = array_merge($this->requiredFields,$this->pahoRequiredFields);
 
-        $this->authChecker->expects($this->atLeast(2))->method('isGranted')->with('ROLE_AMR')->willReturn(true);
+        $this->authChecker->expects($this->atLeast(1))->method('isGranted')->with('ROLE_AMR')->willReturn(true);
         $form = $this->factory->create(CaseType::class);
 
         foreach ($form as $name => $field) {
@@ -59,7 +59,7 @@ class CaseTypeTest extends TypeTestCase
     public function testNonPahoEditingPahoData()
     {
         $fields = array_merge($this->requiredFields,$this->pahoRequiredFields);
-        $this->authChecker->expects($this->once())->method('isGranted')->with('ROLE_AMR')->willReturn(false);
+        $this->authChecker->expects($this->never())->method('isGranted')->with('ROLE_AMR')->willReturn(false);
         $case = new IBD();
         $case->setSite($this->pahoSite);
 
@@ -78,7 +78,7 @@ class CaseTypeTest extends TypeTestCase
 
     public function testNonPneumoniaCountry()
     {
-        $this->authChecker->expects($this->atLeast(2))->method('isGranted')->with('ROLE_AMR')->willReturn(false);
+        $this->authChecker->expects($this->atLeast(1))->method('isGranted')->with('ROLE_AMR')->willReturn(false);
 
         $case = new IBD();
         $case->setSite($this->euroSite);
@@ -167,7 +167,6 @@ class CaseTypeTest extends TypeTestCase
     {
         $extension = new HiddenParentChildExtension();
         $vacType = new VaccinationType();
-        $vacType->setAuthorizationChecker($this->authChecker);
         $types = [new TripleChoice(), new CXRResult(), new CXRAdditionalResult(), new Gender(), new CaseType($this->siteSerializer, $this->groupResolver, $this->authChecker),$vacType];
         return [new PreloadedExtension($types,[ $extension->getExtendedType()=>[$extension]])];
     }
