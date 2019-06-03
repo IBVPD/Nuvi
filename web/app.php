edf83849d1087b8279e52aef41497db8759a5225
Nathanael d. Noblet <nathanael@noblet.ca>
2019-06-03 16:13:33 -0600

    minor updates

--- web/app.php
+++ web/app.php
@@ -1,16 +1,14 @@
 <?php
 
+use Composer\Autoload\ClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
+/** @var ClassLoader $loader */
+$loader = require __DIR__ . '/../app/autoload.php';
+include_once __DIR__ . '/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
-if (PHP_VERSION_ID < 70000) {
-    $kernel->loadClassCache();
-}
-$request = Request::createFromGlobals();
+$kernel   = new AppKernel('live', false);
+$request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
