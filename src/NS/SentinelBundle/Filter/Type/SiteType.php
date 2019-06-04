76d7a67f00b10a34fe617a5dccf8707b32c9d755
Nathanael d. Noblet <nathanael@noblet.ca>
2019-06-04 09:47:44 -0600

    call parent::configureOptions

--- src/NS/SentinelBundle/Filter/Type/SiteType.php
+++ src/NS/SentinelBundle/Filter/Type/SiteType.php
@@ -8,6 +8,7 @@ class SiteType extends BaseObject
 {
     public function configureOptions(OptionsResolver $resolver): void
     {
+        parent::configureOptions($resolver);
         $resolver->setDefaults([
             'choice_label' => 'ajaxDisplay',
             'group_by' => static function ($val, $key, $index) {
