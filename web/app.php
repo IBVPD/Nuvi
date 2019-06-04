9be2b47b94ffb747f44b671060d49a7b19d9172e
Nathanael d. Noblet <nathanael@noblet.ca>
2019-06-04 09:21:40 -0600

    use prod environment

--- web/app.php
+++ web/app.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require __DIR__ . '/../app/autoload.php';
 include_once __DIR__ . '/../var/bootstrap.php.cache';
 
-$kernel   = new AppKernel('live', false);
+$kernel   = new AppKernel('prod', false);
 $request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
