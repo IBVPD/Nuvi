a7447475d0cfd21efa8b0b0cc217e1ea8a2940c2
Nathanael d. Noblet <nathanael@noblet.ca>
2019-06-05 12:02:55 -0600

    Updated ElisaResult to have not-processed option
    Fixes #101

--- src/NS/SentinelBundle/Form/RotaVirus/Types/ElisaResult.php
+++ src/NS/SentinelBundle/Form/RotaVirus/Types/ElisaResult.php
@@ -5,21 +5,20 @@ namespace NS\SentinelBundle\Form\RotaVirus\Types;
 use JMS\TranslationBundle\Translation\TranslationContainerInterface;
 use NS\UtilBundle\Form\Types\TranslatableArrayChoice;
 
-/**
- * Description of ElisaResult
- *
- */
 class ElisaResult extends TranslatableArrayChoice implements TranslationContainerInterface
 {
-    public const NEGATIVE = 0;
-    public const POSITIVE = 1;
-    public const INDETERMINATE = 2;
-    public const UNKNOWN = 99;
+    public const
+        NEGATIVE = 0,
+        POSITIVE = 1,
+        INDETERMINATE = 2,
+        NOT_PROCESSED = 98,
+        UNKNOWN = 99;
 
     protected $values = [
         self::NEGATIVE => 'Negative',
         self::POSITIVE => 'Positive',
         self::INDETERMINATE => 'Indeterminate',
+        self::NOT_PROCESSED => 'Not processed',
         self::UNKNOWN => 'Unknown',
     ];
 }
